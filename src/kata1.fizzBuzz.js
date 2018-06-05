8219fa8bdc3a304d7808ddb90221c6055287bd37
breakfastmeansbreakfast <77744c@gmail.com>
2018-06-05 21:19:00 +0100

    completed fizzbuzz

--- src/kata1.fizzBuzz.js
+++ src/kata1.fizzBuzz.js
@@ -1,5 +1,16 @@
 const fizzBuzz = (number) => {
+const isDividableBy3 = (number % 3) === 0
+const isDividableBy5 = (number % 5) === 0
 
+if (isDividableBy3 && isDividableBy5) {
+return 'FizzBuzz'
 }
-
+if (isDividableBy5) {
+return 'Buzz'
+}
+if (isDividableBy3) {
+    return 'Fizz'
+}
+return number 
+}
 module.exports = fizzBuzz;
