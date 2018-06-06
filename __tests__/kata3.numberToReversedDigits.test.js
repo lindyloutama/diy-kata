860073ff8735a9bb333e95133fb3a6ab4defca3d
breakfastmeansbreakfast <77744c@gmail.com>
2018-06-06 23:49:35 +0100

    completed kata3

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,6 @@ const { numberToReversedDigits } = require('../src');
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-    
+    expect(numberToReversedDigits(12345)).toEqual([1,2,3,4,5])
   });
 });
