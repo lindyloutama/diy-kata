fdb4071cab695a7bf152189bc7b1d21a944b25c4
breakfastmeansbreakfast <77744c@gmail.com>
2018-06-06 22:48:43 +0100

    completed booleans

--- __tests__/kata2.booleanToWord.test.js
+++ __tests__/kata2.booleanToWord.test.js
@@ -1,5 +1,9 @@
 const { booleanToWord } = require('../src');
 
 describe('booleanToWord', () => {
+  it('When booleanToWord is passed true, it should return Yes. When it is passed false, it should return No', () => {
+  expect(booleanToWord(false)).toBe('No')
+  expect(booleanToWord(true)).toBe('Yes')
   // how do we create specs again???
-});
+})
+})
--- src/kata2.booleanToWord.js
+++ src/kata2.booleanToWord.js
@@ -1,5 +1,16 @@
 const booleanToWord = (boolean) => {
+// refectered this 
+    /*
+     if (boolean === true) {
+    return 'Yes'
+} else {
+    return 'No'
+}
+}
+*/
 
+// to this
+return (boolean ? 'Yes' : 'No')
 }
 
 module.exports = booleanToWord;
