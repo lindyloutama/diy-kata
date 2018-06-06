a590dc9d5ea25a8e05c7dfce3243dc20ac2437d2
breakfastmeansbreakfast <77744c@gmail.com>
2018-06-07 00:00:25 +0100

    forgot to reverse the array..

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,6 @@ const { numberToReversedDigits } = require('../src');
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-    expect(numberToReversedDigits(12345)).toEqual([1,2,3,4,5])
+    expect(numberToReversedDigits(12345)).toEqual([5,4,3,2,1])
   });
 });
