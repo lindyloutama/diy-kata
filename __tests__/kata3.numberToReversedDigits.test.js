08c7de26648f397f377973e362b1fd180560b641
Linda <linda.tamakaha@gmail.com>
2018-06-07 23:54:45 +0100

    trial and error reversed digits

--- __tests__/kata3.numberToReversedDigits.test.js
+++ __tests__/kata3.numberToReversedDigits.test.js
@@ -2,6 +2,6 @@ const { numberToReversedDigits } = require('../src');
 
 describe('numberToReversedDigits', () => {
   it('returns a reversed array of the number\'s digits', () => {
-    
+    expect(numberToReversedDigits(1,2,3,4,5)).reverse([5,4,3,2,1])
   });
 });
