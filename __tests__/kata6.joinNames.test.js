67b7e7cdb191b48b66ac50eff78b34a2004a0689
Linda <linda.tamakaha@gmail.com>
2018-06-10 21:38:14 +0100

    JoinNames trial and error

--- __tests__/kata6.joinNames.test.js
+++ __tests__/kata6.joinNames.test.js
@@ -2,6 +2,6 @@ const { joinNames } = require('../src');
 
 describe('joinNames', () => {
   it('returns string of names, seperated by commas and an ampersand', () => {
-    
+    expect(joinNames(names)).toBe(Bart, Lisa & Maggie)
   });
 });
--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,5 +1,12 @@
 const joinNames = (namesObj) => {
-
+  if(namesObj  [{
+    name: 'Bart'
+  }, {
+    name: 'Lisa'
+  }, {
+    name: 'Maggie'
+  }]
+  )
 }
 
 module.exports = joinNames;
