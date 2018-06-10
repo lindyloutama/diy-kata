b67939041f901751caa79c014403adf01d15e3c5
breakfastmeansbreakfast <77744c@gmail.com>
2018-06-10 22:04:51 +0100

    finished kata4, refacting still needed!

--- __tests__/kata4.humanCatDogYears.test.js
+++ __tests__/kata4.humanCatDogYears.test.js
@@ -1,3 +1,9 @@
 const { humanCatDogYears } = require('../src');
 
-// Look Ma, no handlebars!!!
+describe ('humanCatDogYears', () => {
+    it('returns human years for cats and dogs', () => {
+        expect(humanCatDogYears(23)).toEqual([23, 108,129])
+        expect(humanCatDogYears(1)).toEqual([1, 15,15])
+    })
+}
+)
