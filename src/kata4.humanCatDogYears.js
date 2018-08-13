639e0b311fa143ec3cf91cb4e7001a93e39e4075
Linda <linda.tamakaha@gmail.com>
2018-08-13 22:53:38 +0100

    late saves to dit kata

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,10 +1,10 @@
 const humanCatDogYears = (number) => {
 //calculate catYears
 let catYears = 0;
-if (number >= 1 && number < 2) { 
+if (number >= 1 && number < 2) {
 catYears = 15;
 } else if (number === 2) {
-catYears = 24;    
+catYears = 24;
 } else if (number >2) {
 let a = number - 2;
 catYears = (a * 4) + 24;
@@ -15,19 +15,16 @@ return console.log('Not a valid age')
 
 // calculate dogYears
 
-<<<<<<< HEAD
 if (number === 1,2,3,4,5)
     return [10,56,64]
-=======
 let dogYears = 0;
-if (number >= 1 && number < 2) { 
+if (number >= 1 && number < 2) {
 dogYears = 15;
 } else if (number === 2) {
-dogYears = 24;    
+dogYears = 24;
 } else if (number >2) {
 let a = number - 2;
 dogYears = (a * 5) + 24;
->>>>>>> a435d311b0d8854cdbebb0f511a967a6b6797075
 }
 else {
 return console.log('Not a valid age')
